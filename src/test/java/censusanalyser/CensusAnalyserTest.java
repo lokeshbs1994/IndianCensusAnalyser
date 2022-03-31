7d013526845ccfaaecbe709118da17bce4c81516
lokeshbs1994 <lokeshbs619@gmail.com>
2022-03-31 16:14:53 +0530

    ADDED: Code for Testing IndianStatesCode Information From aCSV File

---
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -0,0 +1,44 @@
+package censusanalyser;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+/**
+ * @description create Class for Defining the Indian State Census Data Test
+ *
+ */
+public class CensusAnalyserTest {
+
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    /**
+     * @description create Test Method for IndianCensus Data for collecting Records
+     *   Param: This a Happy Test Case to check the number of Records Match
+     */
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (CensusAnalyserException e) { }
+    }
+
+    /**
+     * @description create Test Method for Indian Census Data for Delimiter Incorrect
+     *  Param: This a Sad Case for Census CSV file is correct but delimiter incorrect returns a custom Exception
+     */
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+}
